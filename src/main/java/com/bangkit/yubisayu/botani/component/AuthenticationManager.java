131647e1fe5bc2785d38bc69d6b66b659da9a35b
anugrah prasetia <anugrahp99@student.ub.ac.id>
2021-05-30 22:54:05 +0700

    create all components

---
+++ src/main/java/com/bangkit/yubisayu/botani/component/AuthenticationManager.java
@@ -0,0 +1,45 @@
+package com.bangkit.yubisayu.botani.component;
+
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+
+@Component
+public class AuthenticationManager implements ReactiveAuthenticationManager {
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    public Mono<Authentication> authenticate(Authentication authentication) {
+        String authToken = authentication.getCredentials().toString();
+
+        try {
+            if (!jwtTokenProvider.validateToken(authToken)) {
+                return Mono.empty();
+            }
+
+            Claims claims = jwtTokenProvider.getAllClaimsFromToken(authToken);
+            String role = claims.get("ROLE", String.class);
+            GrantedAuthority authority = new SimpleGrantedAuthority(role);
+
+            return Mono.just(
+                    new UsernamePasswordAuthenticationToken(
+                            claims.getSubject(),
+                            null,
+                            Collections.singletonList(authority)
+                    )
+            );
+        } catch (Exception e) {
+            return Mono.empty();
+        }
+    }
+}
---
+++ src/main/java/com/bangkit/yubisayu/botani/component/JwtTokenProvider.java
@@ -0,0 +1,92 @@
+package com.bangkit.yubisayu.botani.component;
+
+import com.bangkit.yubisayu.botani.model.entity.user.User;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${app.jwtExpirationInMs}")
+    private Long jwtExpirationTime;
+
+    @Value("${app.jwtRefreshExpirationInMs}")
+    private Long jwtRefreshExpirationTime;
+
+    public String getKey() {
+        return jwtSecret;
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(getKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUserIdFromToken(String token) {
+        return getAllClaimsFromToken(token).getSubject();
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getAllClaimsFromToken(token).getExpiration();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    public String generateAccessToken(User user) {
+        return generateToken(user, jwtExpirationTime);
+    }
+
+    public String generateRefreshToken(User user) {
+        return generateToken(user, jwtRefreshExpirationTime);
+    }
+
+    public String generateToken(User user, Long expiryTime) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("ROLE", user.getRole());
+        return doGenerateToken(claims, user.getId(), expiryTime);
+    }
+
+    public String doGenerateToken(
+            Map<String, Object> claims,
+            String userId,
+            Long expirationTime
+    ) {
+        Date createdDate = new Date();
+        Date expirationDate = new Date(createdDate.getTime() + expirationTime);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userId)
+                .setIssuedAt(createdDate)
+                .setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+    public Boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parse(token);
+            return !isTokenExpired(token);
+        } catch (SignatureException
+                | MalformedJwtException
+                | ExpiredJwtException
+                | UnsupportedJwtException
+                | IllegalArgumentException ex) {
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
